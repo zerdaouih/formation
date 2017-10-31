be2c9b02d247aaa66501ee8d4c334796252fde69
hamada zerdaoui <zerdaoui.hamada@gmail.com>
2017-10-31 01:16:50 +0100

    1st commit

---
+++ src/main/java/com/formation/spring/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.formation.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String homepage() {
+		System.out.println("#### ####");
+		
+		return "index.html";
+		
+	}
+
+}
